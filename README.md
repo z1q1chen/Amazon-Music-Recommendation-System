deef163b8e1b09873e4644157345bc39d467229a
Ziqi Chen <jackychencw@gmail.com>
2021-03-23 18:55:16 -0400

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# MIE1624_Course_Project_Group19
+ MIE1624 Group 19 Course Project
---
+++ baseline.py
@@ -0,0 +1,34 @@
+
+
+from collections import defaultdict
+import pandas as pd
+
+allRatings = []
+userRatings = defaultdict(list)
+
+data = pd.read_csv('train.csv')
+
+allRatings = data['overall'].to_list()
+
+for index_ in data.index:
+    userID = data.loc[index_,'reviewerID']
+    rating = data.loc[index_,'overall']
+    userRatings[userID].append(rating)
+
+globalAverage = sum(allRatings)/len(allRatings)
+userAverage = {}
+for u in userRatings:
+    userAverage[u] = sum(userRatings[u]) / len(userRatings[u])
+
+predictions = open('rating_predictions.csv', 'w')
+for l in open('rating_pairs.csv'):
+    if l.startswith('userID'):
+        #header
+        predictions.write(l)
+        continue
+    u,p = l.strip().split('-')
+    if u in userAverage:
+        
+        predictions.write(u + '-' + p + ',' + str(round(userAverage[u])) + '\n')
+    else:
+        predictions.write(u + '-' + p + ',' + str(round(globalAverage)) + '\n')
